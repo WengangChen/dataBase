9fa2728ef47f038badcb9ba301cf7e65ebcc28e8
52771 <52771@DESKTOP-1TVQMNU>
2016-12-23 14:18:43 +0800

    sql

--- databaseSever/Program.cs
+++ databaseSever/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-using Simple.Owin;
+
 namespace databaseSever
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IPAddress addr = IPAddress.Parse("127.0.0.1");
+            int port = 40400;
+
+            TcpListener listener = new TcpListener(new IPEndPoint(addr, port));
+            listener.Start();
+            while (true)
+            {
+
+                TcpClient remoteClient = listener.AcceptTcpClient();
+
+                NetworkStream streamToClient = remoteClient.GetStream();
+                byte[] buffer = new byte[1024];
+
+                int bytesRead;
+                try
+                {
+                    bytesRead = streamToClient.Read(buffer, 0, buffer.Length);
+                    string command = Encoding.UTF8.GetString(buffer);
+                    DataBaseSql sql = new DataBaseSql();
+                    sql.run(command);
+                }
+                catch (Exception)
+                {
+
+                }
+            }
         }
     }
 }
---
+++ databaseSever/SqlConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Security;
+
+namespace databaseSever
+{
+    class DataBaseSql
+    {
+        System.Data.SqlClient.SqlConnection connection;
+        public DataBaseSql()
+        {
+            //Data Source = localhost; Initial Catalog = 数据库名; User ID = 用户名; Password = 密码
+            string connectString = "Data Source=localHost;Initial Catalog=DataBase;User ID = admin;password = 0";
+            connection = new SqlConnection(connectString);
+            connection.Open();
+        }
+        public void run(string s)
+        {
+            SqlCommand sqlCommand = new SqlCommand(s);
+            sqlCommand.ExecuteNonQuery();
+        }
+         ~DataBaseSql ()
+        {
+            connection.Close();
+        }
+    }
+}
