028a606f445762448beaba0ff0d9b8b0e9d40e08
Wingkou Loeng <961574992@qq.com>
2016-12-23 13:30:45 +0800

    TCP

--- databaseSever/Program.cs
+++ databaseSever/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-using Simple.Owin;
+
 namespace databaseSever
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IPAddress addr = IPAddress.Parse("127.0.0.1");
+            int port = 40400;
+
+            TcpListener listener = new TcpListener(new IPEndPoint(addr, port));
+            listener.Start();
+
+            TcpClient remoteClient = listener.AcceptTcpClient();
+
+            NetworkStream streamToClient = remoteClient.GetStream();
+            byte[] buffer = new byte[1024];
+
+            int bytesRead;
+            try
+            {
+                lock (streamToClient)
+                {
+                    bytesRead = streamToClient.Read(buffer, 0, buffer.Length);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
         }
     }
 }
