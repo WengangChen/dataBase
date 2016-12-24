0d06fcb771d7397cded87867a18eb032c2f56a1b
52771 <52771@DESKTOP-1TVQMNU>
2016-12-24 23:09:57 +0800

    wtf

--- WindowsFormsApplication5/Helper/SqlCombine.cs
+++ WindowsFormsApplication5/Helper/SqlCombine.cs
@@ -55,8 +55,10 @@ namespace WindowsFormsApplication5.Helper
         {
             string x="select 学号, 课程号 into #a from 选课成绩表 where 总评>=60;" + "\n";
             string y = "select 选课成绩表.学号,选课成绩表.课程号 into #b from 选课成绩表,#a where not (选课成绩表.学号=#a.学号 and 选课成绩表.课程号=#a.课程号);" + "\n";
-            string z="select 学号, 课程号 from #b where 课程号 in (select 课程号 from 课程信息表 where 允许补考=1);"+"\n";
-            return x + y + z;
+            string z="select 学号, 课程号 into #c from #b where 课程号 in (select 课程号 from 课程信息表 where 允许补考=1);"+"\n";
+            string xx = "select #c.学号,学生信息表.姓名,#c.课程号 into #d from #c left join 学生信息表 on #c.学号=学生信息表.学号;\n";
+            string yy= "select #d.学号,#d.姓名,#d.课程号,课程信息表.课程名字 from #d left join 课程信息表 on #d.课程号=课程信息表.课程号;";
+            return x + y + z+xx+yy;
         }
         //获取重修的名单
         public static string getCantResitCourse(string studentId)
@@ -64,7 +66,9 @@ namespace WindowsFormsApplication5.Helper
             string x = "select 学号, 课程号 into #a from 选课成绩表 where 总评>=60;" + "\n";
             string y = "select 选课成绩表.学号,选课成绩表.课程号 into #b from 选课成绩表,#a where not (选课成绩表.学号=#a.学号 and 选课成绩表.课程号=#a.课程号);" + "\n";
             string z = "select 学号, 课程号 from #b where 课程号 in (select 课程号 from 课程信息表 where 允许补考=0);" + "\n";
-            return x + y + z;
+            string xx = "select #c.学号,学生信息表.姓名,#c.课程号 into #d from #c left join 学生信息表 on #c.学号=学生信息表.学号;\n";
+            string yy = "select #d.学号,#d.姓名,#d.课程号,课程信息表.课程名字 from #d left join 课程信息表 on #d.课程号=课程信息表.课程号;";
+            return x + y + z + xx + yy;
         }
         //打印补考的成绩
         public static string printResitGrade()
---
+++ WindowsFormsApplication5/View/需要补考的名单.Designer.cs
@@ -0,0 +1,38 @@
+﻿namespace WindowsFormsApplication5.View
+{
+    partial class 需要补考的名单
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Text = "需要补考的名单";
+        }
+
+        #endregion
+    }
+}
